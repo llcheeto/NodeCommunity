b2df7926525e2b817464d6858a5021891555233a
llcheeto <briancornine@gmail.com>
2022-11-19 22:09:06 -0700

    first commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./login.scss";
+
+export default function Login() {
+  return (
+    <div className="login--container">
+      <div>
+        <input />
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Mainpage/Mainpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../Login/Login";
+import Navbar from "../Navbar/Navbar";
+
+export default function Mainpage() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  );
+}
