02746c0307fd1477e448c213ae7dfd72b4214f06
llcheeto <briancornine@gmail.com>
2022-11-21 21:51:44 -0700

    Added the leaflet map for the users node

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import "./login.scss";
+import { TextField, Button } from "@mui/material";
 
 export default function Login() {
   return (
-    <div className="login--container">
-      <div>
-        <input />
-      </div>
+    <div className="login--container login__display">
+      <form onSubmit="">
+        <div>
+          <TextField
+            className="login--field"
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+          />
+          <hr />
+          <TextField
+            className="login--field"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
+          <hr />
+          <div className="login__button">
+            <Button value="submit" variant="outlined">
+              Login
+            </Button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
--- src/Components/Mainpage/Mainpage.jsx
+++ src/Components/Mainpage/Mainpage.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import Login from "../Login/Login";
 import Navbar from "../Navbar/Navbar";
+import "./mainpage.scss";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Mainpage() {
+  // var map = L.map("map").setView([51.505, -0.09], 13);
+
+  // L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  //   maxZoom: 19,
+  //   attribution:
+  //     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  // }).addTo(map);
+
   return (
     <div>
       <Navbar />
-      <Login />
+      <div id="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+
+        <Login />
+      </div>
     </div>
   );
 }
